2e13021d852615578f48e49fc65e8f3f4267d1a1
madellinecoronado <yocarycoronado@gmail.com>
2021-04-23 22:39:28 -0600

    Interfaces del proyecto.

---
+++ src/main/UIController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.awt.event.MouseEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author Pablo
+ */
+public class UIController implements Initializable {
+    
+    @FXML
+    private AnchorPane parent;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void close_app(MouseEvent event){
+        System.exit(0);
+    }
+    
+}
